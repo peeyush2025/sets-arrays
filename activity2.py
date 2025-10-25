9e5bc3a319e94297b3b4cadba98315ee6ea279dd
peeyush2025 <sudhakarln82@gmail.com>
2025-10-25 19:30:52 +0530

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,8 @@
+setx={"green","blue"}
+sety={"blue","yellow"}
+print("orginal set elemants:")
+print(setx)
+print(sety)
+print("\nintersection of two said sets:")
+setz=setx.intersection(sety)
+print(setz)
